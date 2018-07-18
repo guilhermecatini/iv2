921c53cd7c11ff0cd8084b1479ae9d1d45dda86b
Guilherme Catini <guilherme.catini@iv2.com.br>
2018-07-18 11:24:14 -0300

    ok

--- systemparams.js
+++ systemparams.js
@@ -1,8 +1,19 @@
-const globalParams = {
-    accessPort: 8889,
-    dataBaseName: 'iv2tst',
-    protectRoutes: true, 
-    enableRegisterUsers: true
+const env = 'P'; // P = Prdocution / H = Homolog
+
+if (env=='P') {
+    const globalParams = {
+        accessPort: 8888,
+        dataBaseName: 'iv2',
+        protectRoutes: true, 
+        enableRegisterUsers: false
+    }
+} else {
+    const globalParams = {
+        accessPort: 8889,
+        dataBaseName: 'iv2tst',
+        protectRoutes: false, 
+        enableRegisterUsers: true
+    }
 }
 
 module.exports = globalParams;
